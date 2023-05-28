e4a1eee8beab822c56d8f0e8e3fe2654f294e7d4
safatsaat2 <sakibsafat47@gmail.com>
2023-05-28 13:52:58 +0600

    testimonial

--- src/pages/Home/Featured.jsx
+++ src/pages/Home/Featured.jsx
@@ -4,19 +4,22 @@ import '../Home/Featured.css'
 
 const Featured = () => {
     return (
-        <div className="featured-item text-white pt-8 my-20">
-            <SectionTitle subHeading={"check it out"} heading={"Featured Item"}></SectionTitle>
-            <div className="md:flex justify-center items-center px-36 pb-20 pt-12">
-                <div>
-                    <img src={featuredImg} alt="" />
-                </div>
-                <div className="md:ml-10">
-                    <p>Aug 20, 2029</p>
-                    <p className="uppercase">Where can I get some?</p>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim repellendus sit eum sint ipsa vitae officiis repudiandae magnam nam odit ullam magni numquam quia sequi soluta asperiores, saepe, molestias blanditiis.</p>
-                    <button className="btn btn-outline">Read More</button>
+        <div className="featured-item bg-fixed   text-white  my-20">
+            <div className="bg-slate-800 bg-opacity-50 pt-8">
+                <SectionTitle subHeading={"check it out"} heading={"Featured Item"}></SectionTitle>
+                <div className="  md:flex justify-center items-center px-36 pb-20 pt-12">
+                    <div>
+                        <img src={featuredImg} alt="" />
+                    </div>
+                    <div className="md:ml-10">
+                        <p>Aug 20, 2029</p>
+                        <p className="uppercase">Where can I get some?</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim repellendus sit eum sint ipsa vitae officiis repudiandae magnam nam odit ullam magni numquam quia sequi soluta asperiores, saepe, molestias blanditiis.</p>
+                        <button className="btn btn-outline border-0 border-b-4">Read More</button>
+                    </div>
                 </div>
             </div>
+
         </div>
     );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import Banner from "./Banner";
 import Category from "./Category";
 import Featured from "./Featured";
 import PopularMenu from "./PopularMenu";
+import Testimonials from "./Testimonials";
 
 const Home = () => {
     return (
@@ -10,6 +11,7 @@ const Home = () => {
             <Category></Category>
             <PopularMenu></PopularMenu>
             <Featured></Featured>
+            <Testimonials></Testimonials>
         </div>
     );
 };
