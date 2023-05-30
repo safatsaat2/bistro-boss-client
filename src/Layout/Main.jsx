247be3264d61d1cda9850034d3175ea444aeaa67
safatsaat2 <sakibsafat47@gmail.com>
2023-05-30 23:55:00 +0600

    login page added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,14 +1,16 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Navbar from '../pages/Shared/Navbar/Navbar';
 import Footer from '../pages/Shared/Footer/Footer';
 
 
 const Main = () => {
+    const location = useLocation()
+    const isLogin = location.pathname.includes('login')
     return (
         <div>
-            <Navbar></Navbar>
+            { isLogin || <Navbar></Navbar>}
             <Outlet></Outlet>
-            <Footer></Footer>
+            { isLogin || <Footer></Footer>}
         </div>
     );
 };
